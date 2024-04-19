7dac99d22e3b99d7a395459a75e6634e813b3579
Dorijan.Polanc <dorijan.polanc@gmail.com>
2024-04-19 12:15:08 +0200

    Make basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import routes from './routes/routes'
 
 function App() {
     return (
-        <div className="App">
-            <h1>Initial setup</h1>
-        </div>
+        <BrowserRouter basename="/">
+            <Routes>
+                {routes.map((route, index) => (
+                    <Route key={index} path={route.path} element={route.element} />
+                ))}
+            </Routes>
+        </BrowserRouter>
     )
 }
 
---
+++ src/routes/routes.tsx
@@ -0,0 +1,6 @@
+import IRoute from '../types/route.interface'
+import Home from '../pages/Home'
+
+const routes: IRoute[] = [{ path: '/', element: <Home /> }]
+
+export default routes
